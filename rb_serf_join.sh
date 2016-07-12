c3240b259b2ea7526fe572e373d5b8784abda68d
Alberto Rodríguez de la Cruz <arodriguez@redborder.com>
2016-07-12 10:16:04 +0200

    Added comments and fixed bugs

--- rb_serf_join.sh
+++ rb_serf_join.sh
@@ -1,40 +1,88 @@
 #!/bin/bash
 
-SERF_BIN=/root/serf
+#Default values
+SERF_BIN=serf
+ALLOWED_ROLES="master|corezk|core"
+OUR_HOSTNAME=$(hostname -s)
 
+
+#Wrapper for exit command that clean master tags in errors.
+function exit_function() {
+    echo "Cleaning master tag due to error"
+    $SERF_BIN tags -delete master
+    exit $*
+}
+
+#
+# Function to determine if there are any node with master tag. It sets a variable named
+# master_chosen to "yes" if there is a master node or "no", if not.
+# If first argument is setted, function filter members result to find a master with a different
+# name that indicated in the argument, to determine if there are other masters. Result is
+# indicated in another_master variable with yes or no.
+# If there are errors script will be exited.
+#
 function isThereMaster() {
     master_chosen="no"
-    master_members=$($SERF_BIN members -format=json -tag master=yes -status alive)
-    [ "xnull" = "x$(echo $master_members | jq -r .members)" ] || master_chosen="yes"
-}
-function isThereAnotherMaster() {
     another_master="no"
-    other_master_members=$($SERF_BIN members -format=json -tag master=yes -status alive | jq -r .members[].name | grep -v $(hostname -f)| grep -v null)
-    [ "x$other_master_members" = "x" ] || another_master="yes"
+    master_members_result=$($SERF_BIN members -format=json -tag master=yes -status alive)
+    if [ $? -eq 0 ] ; then
+        [ "xnull" = "x$(echo $master_members_result | jq -r .members)" ] || master_chosen="yes"
+        if [ "x$1" != "x" -a "x$master_chosen" = "xyes" ] ; then
+            other_master_names=$(echo $master_members_result | jq -r .members[].name)
+            if [ $? -eq 0 ] ; then
+                echo $other_master_names | tr ' ' '\n' | grep -v $1 > /dev/null
+                [ $? -eq 0 ] && another_master="yes"
+            else
+                echo "Error parsing master members result"
+                echo $other_master_names
+                exit_function 2
+            fi
+        fi
+    else
+        echo "Error in serf members command when trying to get master nodes!!"
+        echo $master_members_result
+        exit_function 1;
+    fi
 }
 
-#Function to get lowest IP registered by Serf. You can add -tag params to filter
+#
+# Function to get lowest IP registered by Serf. You can add -tag params to filter by tag the result.
+# If there are errors, script will be exited.
+#
 function lowestIP() {
-    lowest_ip=$($SERF_BIN members $* -format=json -status alive | jq -r .members[].addr | sort | awk '{split($0,a,":"); print a[1]}' | head -n1)
+    ip_members_result=$($SERF_BIN members $* -format=json -status alive)
+    if [ $? -eq 0 ] ; then
+        ip_list=$(echo $ip_members_result | jq -r .members[].addr)
+        if [ $? -eq 0 ] ; then
+            lowest_ip=$(echo $ip_list | tr ' ' '\n' | sort | awk '{split($0,a,":"); print a[1]}' | head -n1)
+        else
+            echo "Error parsing serf members response"
+            exit_function 3;
+        fi
+    else
+        echo "Error in serf members command when trying to get lowest ip!!"
+        echo $ip_members_result
+    fi
 }
 
 #Usage function
 function usage() {
-    echo "TODO USAGE"
+    echo "rb_serf_join.sh <OPTIONS>"
+    echo "  h) usage"
+    echo "  r) allowed roles: regex with roles that can be cluster masters By default: master|corezk|core"
 }
 
 #Getting options
-while getopts "" opt ; do
+while getopts "hr:o:" opt ; do
     case $opt in
-        h) usage;
+        h) usage;;
+        r) ALLOWED_ROLES=$OPTARGS;;
+        o) OUR_HOSTNAME=$OPTARGS;;
     esac
 done
 
-#Check if serf agent is running ; TODO!!
+#Check if serf agent is running
 #TODO: use systemd to get serf status
-
-#Join to cluster
-#$SERF_BIN join
 
 #Is there master?
 isThereMaster
@@ -42,24 +90,25 @@ isThereMaster
 if [ "x$master_chosen" = "xyes" ] ; then
     echo "There is already a master node"
 else
-    lowestIP #-tag role="master|corezk|core"
+    lowestIP -tag role="$ALLOWED_ROLES"
     #TODO: GET MY IP (ESTE ES DE PRUEBA)
     my_ip=$(ip a s bond0 2>/dev/null |grep inet|grep brd|awk '{print $2}'|head -n 1|tr '/' ' '|awk '{print $1}')
     #If there is a node with lowest ip, i am not a master node
     if [ "x$my_ip" != "x$lowest_ip" ] ; then
-         $SERF_BIN tags -delete master
-         echo "This node is not master"
+        $SERF_BIN tags -delete master
+        echo "This node is not master"
     else
         #If i am the node with lowest ip, i am a temporal master
-	$SERF_BIN tags -set master=yes
+	      $SERF_BIN tags -set master=yes
         EXIT=no
-        while [ "x$EXIT" = "xno"  ] ; do
+        counter=0
+        while [ "x$EXIT" = "xno" -a $counter -le 10 ] ; do
             #Wait to detect if there is another recent master
             sleep 2
             #Is there another master?
-            isThereAnotherMaster
+            isThereMaster $OUR_HOSTNAME
             if [ "x$another_master" = "xno" ] ; then
-	        echo "This node is the new master";
+	              echo "This node is the new master";
                 EXIT=yes
             else
                 #Am I the lowest IP of master nodes?
@@ -71,6 +120,7 @@ else
                     EXIT=yes
                 fi
             fi
+            let counter=counter+1
         done
     fi
 fi
